91adf0a0761e3f2db7f4536675a1f3e829dfd3b3
acongacong715 <acongacong715@gmail.com>
2026-08-29 14:10:36 +0700

    Add index.ts to export all repositories

---
+++ src/database/repositories/index.ts
@@ -0,0 +1,10 @@
+export { SettingsRepository, default as SettingsRepositoryDefault } from './settings.repository';
+export { WalletRepository, default as WalletRepositoryDefault } from './wallet.repository';
+export { TransactionRepository, default as TransactionRepositoryDefault } from './transaction.repository';
+export { TokenRepository, default as TokenRepositoryDefault } from './token.repository';
+
+// Re-export for convenience
+export type { SettingsRepository } from './settings.repository';
+export type { WalletRepository } from './wallet.repository';
+export type { TransactionRepository } from './transaction.repository';
+export type { TokenRepository } from './token.repository';
