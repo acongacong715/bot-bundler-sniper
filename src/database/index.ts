a44f00f0435fd485d9203fd6a66524d6462b10ae
acongacong715 <acongacong715@gmail.com>
2026-08-29 14:12:41 +0700

    Add index.ts to database folder for centralized exports

---
+++ src/database/index.ts
@@ -0,0 +1,8 @@
+export { DatabaseService, default as DatabaseServiceDefault } from './database.service';
+export { DatabaseManager, default as DatabaseManagerDefault } from './database';
+
+// Re-export repositories
+export * from './repositories';
+
+// Export types
+export type { DatabaseService } from './database.service';
